47a2b3f10b1664c0a19be79052f54bd3b7fdf9c4
prshntiwri <prashanttiwari6749@gmail.com>
2025-08-29 11:41:45 +0530

    Runtime Error

---
+++ kietoops/src/KIET_OOPS/Exception_Handling/InsufficientBalanceException.java
@@ -0,0 +1,7 @@
+package KIET_OOPS.Exception_Handling;
+
+public class InsufficientBalanceException  extends RuntimeException{
+    public InsufficientBalanceException(String message) {
+        super(message);
+    }
+}
